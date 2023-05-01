43c4332513d7ae91f2d6cc3262d9b823612ec756
scs0209 <110822847+scs0209@users.noreply.github.com>
2023-05-01 16:53:13 +0900

    Initial commit

---
+++ scripts/footer.js
@@ -0,0 +1,8 @@
+"use strict";
+const footer = document.getElementById("footer");
+
+footer.innerHTML = `
+  <div class="container">
+    <p>&copy; 2023 Shopping Mall Recommendation</p>
+  </div>
+`;
